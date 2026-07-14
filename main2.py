500bd12b58374c74d2548c7daa8e4497f801b0f3
tommylee9068 <tommylee9068@gmail.com>
2026-07-14 17:29:35 +0900

    6일차

---
+++ main2.py
@@ -0,0 +1,7 @@
+
+
+import my_shapes.area as ma
+
+print("■ 커스텀 패키지 실행 결과 ■")
+print(f"반지름 5인 원의 넓이 : {ma.circle_area(5)}")       #출력: 78.5
+print(f"가로4 세로5 사각형 넓이 : {ma.square_area(4, 5)}")   #출력: 20
---
+++ my_shapes/area.py
@@ -0,0 +1,8 @@
+
+
+def circle_area(r):
+      return 3.14 * r * r
+
+
+def square_area(w, h):
+      return w * h
